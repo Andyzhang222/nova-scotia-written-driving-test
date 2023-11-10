3e59eebc29e40b0f92a9504440f23374dfb8b6ff
locojk <gaojun5150@gmail.com>
2023-11-10 19:59:58 -0400

    Refactor DrivingTestActivity

--- app/src/main/java/com/example/novascotiawrittendrivingtest/DrivingTestActivity.kt
+++ app/src/main/java/com/example/novascotiawrittendrivingtest/DrivingTestActivity.kt
@@ -23,16 +23,30 @@ class DrivingTestActivity : AppCompatActivity() {
     private lateinit var pbProgress: ProgressBar
     private lateinit var tvProgress: TextView
 
-    private lateinit var mQuestionsList: ArrayList<Question>
+    private lateinit var questionsList: ArrayList<Question>
 
-    private var mSelectedPosition: Int = 0
-    private var mCorrectAnswer: Int = 0
-    private var mCurrentPosition: Int = 1
+    private var selectedPosition: Int = 0
+    private var correctAnswer: Int = 0
+    private var currentPosition: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.driving_test_layout)
 
+        initializeViews()
+
+        questionsList = Constants.getQuestion()
+        initializeQuestion()
+
+        tvOptionOne.setOnClickListener { setOptionClick(it) }
+        tvOptionTwo.setOnClickListener { setOptionClick(it) }
+        tvOptionThree.setOnClickListener { setOptionClick(it) }
+        tvOptionFour.setOnClickListener { setOptionClick(it) }
+
+        btnSubmit.setOnClickListener { setOptionClick(it) }
+    }
+
+    private fun initializeViews() {
         tvQuestion = findViewById(R.id.tv_question)
         imageView = findViewById(R.id.imageView)
         tvOptionOne = findViewById(R.id.tv_optionOne)
@@ -42,29 +56,10 @@ class DrivingTestActivity : AppCompatActivity() {
         btnSubmit = findViewById(R.id.btnSubmit)
         pbProgress = findViewById(R.id.pb)
         tvProgress = findViewById(R.id.tv_progress)
-
-        mQuestionsList = Constants.getQuestion()
-        setQuestion()
-
-        tvOptionOne.setOnClickListener {
-            setOptionClick(it)
-        }
-        tvOptionTwo.setOnClickListener {
-            setOptionClick(it)
-        }
-        tvOptionThree.setOnClickListener {
-            setOptionClick(it)
-        }
-        tvOptionFour.setOnClickListener {
-            setOptionClick(it)
-        }
-        btnSubmit.setOnClickListener {
-            setOptionClick(it)
-        }
     }
 
-    private fun setQuestion() {
-        val question: Question = mQuestionsList[mCurrentPosition - 1]
+    private fun initializeQuestion() {
+        val question: Question = questionsList[currentPosition - 1]
 
         tvQuestion.text = question.question
         imageView.setImageResource(question.image)
@@ -73,77 +68,80 @@ class DrivingTestActivity : AppCompatActivity() {
         tvOptionThree.text = question.optionThree
         tvOptionFour.text = question.optionFour
 
-        pbProgress.progress = mCurrentPosition
-        tvProgress.text = "$mCurrentPosition / ${pbProgress.max}"
+        pbProgress.progress = currentPosition
+        tvProgress.text = "$currentPosition / ${pbProgress.max}"
 
-        defaultAppearance()
+        setDefault(tvOptionOne)
+        setDefault(tvOptionTwo)
+        setDefault(tvOptionThree)
+        setDefault(tvOptionFour)
 
-        if (mCurrentPosition == mQuestionsList.size) {
-            btnSubmit.text = "Finish Quiz"
-        } else {
-            btnSubmit.text = "Submit"
-        }
+        btnSubmit.text = if (currentPosition == questionsList.size) "Finish Quiz" else "Answer"
     }
 
-    private fun defaultAppearance() {
-        val options = ArrayList<TextView>()
-        options.add(0, tvOptionOne)
-        options.add(1, tvOptionTwo)
-        options.add(2, tvOptionThree)
-        options.add(3, tvOptionFour)
-
-        for (option in options) {
-            option.setTextColor(Color.parseColor("#7A8089"))
-            option.typeface = Typeface.DEFAULT
-            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
-        }
+    private fun setDefault(v: TextView) {
+        v.setTextColor(Color.parseColor("#7A8089"))
+        v.typeface = Typeface.DEFAULT
+        v.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
     }
 
-    fun setOptionClick(v: View?) {
+    private fun setOptionClick(v: View?) {
         when (v?.id) {
             R.id.tv_optionOne -> selectedOptionView(tvOptionOne, 1)
             R.id.tv_optionTwo -> selectedOptionView(tvOptionTwo, 2)
             R.id.tv_optionThree -> selectedOptionView(tvOptionThree, 3)
             R.id.tv_optionFour -> selectedOptionView(tvOptionFour, 4)
-            R.id.btnSubmit -> {
-                if (mSelectedPosition == 0) {
-                    mCurrentPosition++
-                    when {
-                        mCurrentPosition <= mQuestionsList.size -> setQuestion()
-//                        else -> navigateToScore()
-                    }
-                } else {
-                    val question = mQuestionsList[mCurrentPosition - 1]
-                    if (question.correctAnswer != mSelectedPosition) {
-                        answerView(mSelectedPosition, R.drawable.wrong_option_border_bg)
-                    } else {
-                        mCorrectAnswer++
-                    }
-
-                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
-
-                    if (mCurrentPosition == mQuestionsList.size) {
-                        btnSubmit.text = "Finish Quiz"
-                    } else {
-                        btnSubmit.text = "Next Question"
-                    }
-                    mSelectedPosition = 0
-                }
-            }
+            R.id.btnSubmit -> handleSubmitButtonClicked()
         }
+    }
+
+    private fun handleSubmitButtonClicked() {
+        if (selectedPosition == 0) {
+            handleNoOptionSelected()
+        } else {
+            evaluateSelectedOption()
+        }
+    }
+
+    private fun handleNoOptionSelected() {
+        currentPosition++
+        if (currentPosition <= questionsList.size) {
+            initializeQuestion()
+        } else {
+            // navigateToScore() // Uncomment or implement this when needed
+        }
+    }
+
+    private fun evaluateSelectedOption() {
+        val question = questionsList[currentPosition - 1]
+        if (question.correctAnswer != selectedPosition) {
+            answerUpdate(selectedPosition, R.drawable.wrong_option_border_bg)
+        } else {
+            correctAnswer++
+        }
+
+        answerUpdate(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+        btnSubmit.text = if (currentPosition == questionsList.size) "Finish Quiz" else "Next Question"
+
+        selectedPosition = 0
     }
 
 
     private fun selectedOptionView(tv: TextView, selectedPosition: Int) {
-        defaultAppearance()
-        mSelectedPosition = selectedPosition
+        setDefault(tvOptionOne)
+        setDefault(tvOptionTwo)
+        setDefault(tvOptionThree)
+        setDefault(tvOptionFour)
+
+        this.selectedPosition = selectedPosition
 
         tv.setTextColor(Color.parseColor("#363A43"))
         tv.setTypeface(tv.typeface, Typeface.BOLD)
         tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
     }
 
-    private fun answerView(selectedOption: Int, drawableView: Int) {
+    private fun answerUpdate(selectedOption: Int, drawableView: Int) {
         when (selectedOption) {
             1 -> tvOptionOne.background = ContextCompat.getDrawable(this, drawableView)
             2 -> tvOptionTwo.background = ContextCompat.getDrawable(this, drawableView)
