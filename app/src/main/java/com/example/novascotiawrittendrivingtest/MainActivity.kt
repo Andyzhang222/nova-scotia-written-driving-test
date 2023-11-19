f42e80e7bcc0ad0c48c362e79b2b4b822aa51f9d
Ashley(Zhuyun) Zhang <zh631622@dal.ca>
2023-11-19 19:35:59 -0400

    add log out tool bat to main activity

--- app/src/main/java/com/example/novascotiawrittendrivingtest/MainActivity.kt
+++ app/src/main/java/com/example/novascotiawrittendrivingtest/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.novascotiawrittendrivingtest
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.ProgressBar
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var progressBar: ProgressBar
     private var questionCount = 0
-
+    private lateinit var navToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        initialToolBar()
         progress()
     }
 
@@ -25,5 +29,29 @@ class MainActivity : AppCompatActivity() {
         fun run() {
             progressBar.progress = questionCount
         }
+    }
+
+    private fun initialToolBar()
+    {
+        navToolbar = findViewById(R.id.main_toolbar)
+        setSupportActionBar(navToolbar)
+        supportActionBar?.setTitle("")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        when (item.itemId) {
+            R.id.action_log_out -> {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
