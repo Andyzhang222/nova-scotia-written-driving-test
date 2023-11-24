5de77fceac6b7d0d836902e4b29b4754a917eb8d
locojk <gaojun5150@gmail.com>
2023-11-24 15:46:14 -0400

    Change dependency conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,13 @@ dependencies {
     // Zhuyun add for realtime database library
     implementation("com.google.firebase:firebase-database")
 
+    // Map and Location
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
     // Jun Gao add for retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
